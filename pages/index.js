fa0af135f391c9b442d4e722865f2f7d41ec8b69
Erick D. Calazans <43283862+ecalazans@users.noreply.github.com>
2024-11-04 01:59:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 function Home() {
-  return <>
-    <h1>🚀 Parabéns!!!</h1>
-    <h2>Pra você que lê essa mensagem: Você é D+!!! Sucesso em sua vida!!! 😍</h2>
-  </>
+  return (
+    <>
+      <h1>🚀 Parabéns!!!</h1>
+      <h2>
+        Pra você que lê essa mensagem: Você é D+!!! Sucesso em sua vida!!! 😍
+      </h2>
+    </>
+  );
 }
 
-export default Home
+export default Home;
